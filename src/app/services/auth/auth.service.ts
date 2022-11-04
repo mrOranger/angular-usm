4b697d9528788805c7182c1634a5ec458288b489
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-04 09:41:13 +0100

    Created events to emit

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Output, EventEmitter } from '@angular/core';
+import User from 'src/app/models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  public static readonly TOKEN : string= 'token';
+  public static readonly TOKEN: string = 'token';
+  @Output('login') public userLogin: EventEmitter<User>;
+  @Output('register') public userSignIn: EventEmitter<User>;
+  @Output('logout') public userLogout: EventEmitter<any>;
 
   constructor() { 
+    this.userLogin = new EventEmitter<User>();
+    this.userSignIn = new EventEmitter<User>();
+    this.userLogout = new EventEmitter();
   }
 
   public isUserLoggedIn(): boolean {
@@ -19,6 +26,11 @@ export class AuthService {
   */
   public login(email: string, password: string): boolean {
     localStorage.setItem(AuthService.TOKEN, email);
+
+    /* Once the user login, the event login is emitted */
+    const testUser: User = new User(email, '', '', new Date(), '');
+    this.userLogin.emit(testUser);
+
     return true;
   }
 
@@ -27,6 +39,11 @@ export class AuthService {
   */
   public register(username: string, email: string, password: string): boolean {
     localStorage.setItem(AuthService.TOKEN, email);
+
+    /* Once the user signIn, the event signIn is emitted */
+    const testUser: User = new User(email, '', '', new Date(), '');
+    this.userSignIn.emit(testUser);
+
     return true;
   }
 
@@ -35,6 +52,10 @@ export class AuthService {
   */
   public logout(): boolean {
     localStorage.removeItem(AuthService.TOKEN);
+
+    /* Once the user logout, the event logout is emitted */
+    this.userLogout.emit();
+
     return true;
   }
 }

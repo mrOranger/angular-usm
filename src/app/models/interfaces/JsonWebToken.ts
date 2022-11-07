e988703a6901174d883280889e47d2e11842b271
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-07 11:51:49 +0100

    Add feature pof JWT

---
+++ src/app/models/interfaces/JsonWebToken.ts
@@ -0,0 +1,7 @@
+export interface JsonWebToken {
+    access_token: string,
+    token_type: string,
+    expires_in: number,
+    first_name: string,
+    email: string
+};
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, Output, EventEmitter } from '@angular/core';
+import { JsonWebToken } from 'src/app/models/interfaces/JsonWebToken';
 import User from 'src/app/models/User';
 
 @Injectable({
@@ -6,15 +8,21 @@ import User from 'src/app/models/User';
 })
 export class AuthService {
 
+  public static readonly API_AUTH: string = 'http://localhost:8000/api/auth/';
   public static readonly TOKEN: string = 'token';
+
   @Output('login') public userLogin: EventEmitter<User>;
   @Output('register') public userSignIn: EventEmitter<User>;
   @Output('logout') public userLogout: EventEmitter<any>;
 
-  constructor() { 
+  private httpClient: HttpClient;
+
+  constructor(httpClient: HttpClient) { 
     this.userLogin = new EventEmitter<User>();
     this.userSignIn = new EventEmitter<User>();
     this.userLogout = new EventEmitter();
+
+    this.httpClient = httpClient;
   }
 
   public isUserLoggedIn(): boolean {
@@ -25,11 +33,24 @@ export class AuthService {
     Used for query the db and authorize the user
   */
   public login(email: string, password: string): boolean {
-    localStorage.setItem(AuthService.TOKEN, email);
+    
+    this.httpClient.post(AuthService.API_AUTH + 'login', {
+      email: email,
+      password: password
+    }).subscribe((payload: any) => {
+      
+      localStorage.setItem(AuthService.TOKEN, payload.access_token);
+      localStorage.setItem('users', JSON.stringify(payload));
 
-    /* Once the user login, the event login is emitted */
-    const testUser: User = new User('', email, '', '', new Date(), '');
-    this.userLogin.emit(testUser);
+      /* Once the user login, the event login is emitted */
+      const user: User = new User('', payload.email, payload.first_name, '', new Date(), '');
+      this.userLogin.emit(user);
+
+      return true;
+    }, (httpRespone: HttpErrorResponse) => {
+      alert(httpRespone.message);
+      return false;
+    });
 
     return true;
   }

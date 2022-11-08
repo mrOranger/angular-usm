8857ff0af17312a05064647e8b9c57763cf54589
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-08 13:19:24 +0100

    Modified component register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -13,6 +13,12 @@ export class RegisterComponent implements OnInit {
   private authService: AuthService;
   private router: Router;
 
+  private firstName: string;
+  private lastName: string;
+  private dateOfBirth: Date;
+  private email: string;
+  private password: string;
+
   constructor(authService : AuthService, router : Router) { 
     this.authService = authService;
     this.router = router;
@@ -23,16 +29,35 @@ export class RegisterComponent implements OnInit {
   }
 
   public signUp(form: NgForm): void {
-    const username: string = form.value.username;
-    const email: string = form.value.email;
-    const password: string = form.value.password;
-    if (this.authService.register(username, email, password)) {
+    this.readData(form);
+    console.log('First name: ' + this.firstName);
+    console.log('Last name: ' + this.lastName);
+    console.log('Date of birth: ' + this.dateOfBirth);
+    console.log('Email: ' + this.email);
+    console.log('Password: ' + this.password);
+    console.log('TaxCode: ' + this.generateTaxCode());
+
+    /*if (this.authService.register(username, email, password)) {
       this.router.navigate(['users']);
-    }
+    }*/
   }
 
   public signIn(): void {
     this.router.navigate(['login']);
+  }
+
+  private readData(form: NgForm): void {
+    this.firstName = form.value.firstName;
+    this.lastName = form.value.lastName;
+    this.dateOfBirth = new Date(form.value.dateOfBirth);
+    this.email = form.value.email;
+    this.password = form.value.password;
+  }
+
+  private generateTaxCode(): string {
+    return this.firstName.substring(0, 3).toUpperCase()
+      .concat(this.lastName.substring(0, 3).toUpperCase())
+      .concat(this.dateOfBirth.getFullYear().toString());
   }
 
 }

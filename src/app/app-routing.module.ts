b0b4bb216d0a36d150dc26a1c170454a2aafef8b
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-14 10:18:43 +0100

    Removed old routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
-import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UserDataComponent } from './user-data/user-data.component';
 import { RouteGuardsService } from './services/route-guards/route-guards.service';
 import { RegisterComponent } from './register/register.component';
@@ -20,14 +19,6 @@ const routes: Routes = [{
 }, {
   path: 'users/:id',
   component: UserDataComponent,
-  canActivate: [RouteGuardsService]
-}, {
-  path: 'users/new',
-  component: UserDetailComponent,
-  canActivate: [RouteGuardsService]
-}, {
-  path: 'users/:id/edit',
-  component: UserDetailComponent,
   canActivate: [RouteGuardsService]
 }, {
   path: '',
